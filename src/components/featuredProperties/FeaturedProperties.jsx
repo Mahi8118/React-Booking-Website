55749bb402e1fca2a12af27179aea09bda0b5d5d
mahesh <mahesh@gmail.com>
2023-12-28 16:04:19 +0530

    commit change

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -5,7 +5,7 @@ const FeaturedProperties = () => {
     <div className="fp">
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          src="https://th.bing.com/th/id/OIP.VHbV9i9VvkaCBYQHfMNZ3QHaHa?w=191&h=192&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           alt=""
           className="fpImg"
         />
@@ -19,7 +19,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
+          src="https://th.bing.com/th/id/OIP.CWMobkWSUEAofBDc3bpEFgHaEU?w=322&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           alt=""
           className="fpImg"
         />
@@ -33,7 +33,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/232902339.jpg?k=3947def526b8af0429568b44f9716e79667d640842c48de5e66fd2a8b776accd&o=&hp=1"
+          src="https://th.bing.com/th/id/OIP.Odfp54s2vkoDDb56M6PU7gHaJZ?w=159&h=201&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           alt=""
           className="fpImg"
         />
@@ -47,7 +47,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1"
+          src="https://th.bing.com/th/id/OIP.bMsOLnRhvVFyQ70OWZbm9AHaFI?w=266&h=184&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           alt=""
           className="fpImg"
         />
